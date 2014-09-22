0bb93a48babee8cfe30207ebdee43765491735ba
Daniel Tschan <tschan@puzzle.ch>
2014-09-22 23:20:56 +0200

    Added OpenShift configuration.

--- openshift/install.sh
+++ openshift/install.sh
@@ -1,5 +1,16 @@
 #! /bin/sh
 
+test -e $OPENSHIFT_DATA_DIR/.install && exit 0
+
 touch $OPENSHIFT_DATA_DIR/.install
+
+source $OPENSHIFT_HOMEDIR/python/virtenv/bin/activate
+
+sed -e 's/Django[<>=].*/Django==1.6/' $OPENSHIFT_REPO_DIR/requirements.txt >/tmp/requirements.txt
+
 > $OPENSHIFT_DATA_DIR/install.log
-gear build >$OPENSHIFT_DATA_DIR/install.log && gear stop && gear start && touch $OPENSHIFT_DATA_DIR/.installed && rm OPENSHIFT_DATA_DIR/.install
+
+pip install -r /tmp/requirements.txt && \
+ctl_all restart && \
+touch $OPENSHIFT_DATA_DIR/.installed && \
+rm OPENSHIFT_DATA_DIR/.install
