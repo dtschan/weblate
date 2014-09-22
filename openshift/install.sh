4e7522d303ad51c6fa5059d0df6b166c3d429610
Daniel Tschan <tschan@puzzle.ch>
2014-09-23 00:14:47 +0200

    Added OpenShift configuration.

--- openshift/install.sh
+++ openshift/install.sh
@@ -1,5 +1,8 @@
 #! /bin/sh
 
+set -e
+trap "rm $OPENSHIFT_DATA_DIR/.install" EXIT
+
 test -e $OPENSHIFT_DATA_DIR/.install && exit 0
 
 touch $OPENSHIFT_DATA_DIR/.install
@@ -10,8 +13,44 @@ sed -e 's/Django[<>=].*/Django==1.6/' $OPENSHIFT_REPO_DIR/requirements.txt >/tmp
 
 > $OPENSHIFT_DATA_DIR/install.log
 
-pip install -r /tmp/requirements.txt >$OPENSHIFT_DATA_DIR/install.log && \
+pip install -r /tmp/requirements.txt >$OPENSHIFT_DATA_DIR/install.log &&
+
+if [ ! -s $OPENSHIFT_DATA_DIR/weblate.db ]; then
+  rm -f ${OPENSHIFT_DATA_DIR}/CREDENTIALS
+fi
+
+if [ ! -s $OPENSHIFT_REPO_DIR/weblate/fixtures/initial_data.json ]; then
+  mkdir -p $OPENSHIFT_REPO_DIR/weblate/fixtures
+  cat <<-EOF >$OPENSHIFT_REPO_DIR/weblate/fixtures/initial_data.json
+    [{
+        "pk": 1,
+        "model": "sites.site",
+        "fields": {
+            "name": "${OPENSHIFT_APP_DNS}",
+            "domain":"${OPENSHIFT_APP_DNS}"
+        }
+    }]
+  EOF
+fi
+
+echo "Executing 'python $OPENSHIFT_REPO_DIR/openshift/manage.py syncdb --noinput'"
+python "$OPENSHIFT_REPO_DIR"openshift/manage.py syncdb --noinput
+
+echo "Executing 'python $OPENSHIFT_REPO_DIR/openshift/manage.py migrate'"
+python "$OPENSHIFT_REPO_DIR"openshift/manage.py migrate
+
+echo "Executing 'python $OPENSHIFT_REPO_DIR/openshift/manage.py collectstatic --noinput'"
+python "$OPENSHIFT_REPO_DIR"openshift/manage.py collectstatic --noinput
+
+if [ ! -s $OPENSHIFT_DATA_DIR/CREDENTIALS ]; then
+  echo "Executing 'python $OPENSHIFT_REPO_DIR/openshift/manage.py createadmin'"
+  python "$OPENSHIFT_REPO_DIR"openshift/manage.py createadmin
+
+  echo "Generating Django Admin credentials and writing them to ${OPENSHIFT_DATA_DIR}/CREDENTIALS"
+  python "$OPENSHIFT_REPO_DIR".openshift/action_hooks/secure_db.py | tee ${OPENSHIFT_DATA_DIR}/CREDENTIALS
+fi
+
+touch $OPENSHIFT_DATA_DIR/.installed && \
 gear stop && \
 gear start && \
-touch $OPENSHIFT_DATA_DIR/.installed && \
 rm OPENSHIFT_DATA_DIR/.install
