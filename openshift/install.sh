8d98830f787fb905a54cc93e46b8d874aff6e4ac
Daniel Tschan <tschan@puzzle.ch>
2014-09-22 21:33:26 +0200

    Added OpenShift configuration.

--- openshift/install.sh
+++ openshift/install.sh
@@ -1,4 +1,5 @@
 #! /bin/sh
 
 touch $OPENSHIFT_DATA_DIR/.install
+> $OPENSHIFT_DATA_DIR/install.log
 gear build >$OPENSHIFT_DATA_DIR/install.log && gear stop && gear start && touch $OPENSHIFT_DATA_DIR/.installed && rm OPENSHIFT_DATA_DIR/.install
