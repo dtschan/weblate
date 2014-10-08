d2bacd6c6eb6aeed01f64592a5b98ca9a827f9b7
N. Harrison Ripps <hripps@redhat.com>
2014-10-08 21:12:59 +0200

    Copied openshiftlibs.py and secure_db.py from https://github.com/openshift/django-example.

---
+++ .openshift/action_hooks/secure_db.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+
+# Copyright 2011-2014 Red Hat Inc. and/or its affiliates and other contributors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,  
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import hashlib, imp, os, sqlite3
+
+# Load the OpenShift helper library
+lib_path      = os.environ['OPENSHIFT_REPO_DIR'] + 'wsgi/openshift/'
+modinfo       = imp.find_module('openshiftlibs', [lib_path])
+openshiftlibs = imp.load_module('openshiftlibs', modinfo[0], modinfo[1], modinfo[2])
+
+# Open the database
+conn = sqlite3.connect(os.environ['OPENSHIFT_DATA_DIR'] + '/sqlite3.db')
+c    = conn.cursor()
+
+# Grab the default security info
+c.execute('SELECT password FROM AUTH_USER WHERE id = 1')
+pw_info = c.fetchone()[0]
+
+# The password is stored as [hashtype]$[salt]$[hashed]
+pw_fields = pw_info.split("$")
+hashtype  = pw_fields[0]
+old_salt  = pw_fields[1]
+old_pass  = pw_fields[2]
+
+# Randomly generate a new password and a new salt
+# The PASSWORD value below just sets the length (12)
+# for the real new password.
+old_keys = { 'SALT': old_salt, 'PASS': '123456789ABC' }
+use_keys = openshiftlibs.openshift_secure(old_keys)
+
+# Encrypt the new password
+new_salt    = use_keys['SALT']
+new_pass    = use_keys['PASS']
+new_hashed  = hashlib.sha1(new_salt + new_pass).hexdigest()
+new_pw_info = "$".join([hashtype,new_salt,new_hashed])
+
+# Update the database
+c.execute('UPDATE AUTH_USER SET password = ? WHERE id = 1', [new_pw_info])
+conn.commit()
+c.close()
+conn.close()
+
+# Print the new password info
+print "Django application credentials:\n\tuser: admin\n\t" + new_pass
