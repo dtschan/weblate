5bee2430ef57f4ec9a5ad55c0d2248d9ec7efe1f
Daniel Tschan <tschan@puzzle.ch>
2014-10-17 13:55:51 +0200

    Added OpenShift configuration.

--- openshift/update.sh
+++ openshift/update.sh
@@ -11,7 +11,10 @@ BRANCH="${2:-master}"
 
 OLD_HEAD=`git rev-parse "$BRANCH"`
 cd ${OPENSHIFT_HOMEDIR}/git/${OPENSHIFT_APP_NAME}.git
-git fetch "$URL" "$BRANCH"
+echo git fetch "$URL" "$BRANCH"
+pwd
+
+exit 0
 
 if ! git cat-file -e "$BRANCH":.openshift 2>/dev/null; then
   echo "Fatal error: Branch $BRANCH of repository at $URL doesn't contain an OpenShift configuration!" >&2
