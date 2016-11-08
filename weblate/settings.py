1017d6f5de6fa2eb92370a562e737ce8e4c05db4
Daniel Tschan <tschan@puzzle.ch>
2016-11-08 21:20:24 +0100

    Update OpenShift 3 module.

--- weblate/settings.py
+++ weblate/settings.py
@@ -168,8 +168,8 @@ STATICFILES_DIRS = (
     # Don't forget to use absolute paths, not relative paths.
 )
 
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
-#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # List of finder classes that know how to find static files in
 # various locations.
